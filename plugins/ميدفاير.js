0c4e62e0d7f2e52a8e2cce59a45705fb75ee64ab
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 11:23:45 +0200

    Create ميدفاير.js

---
+++ plugins/ميدفاير.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `${lenguajeGB['smsAvisoMG']()}_*نسيت تحط الينك*_`
+try {  
+let res = await mediafireDl(args[0])
+let { name, size, date, mime, link } = res
+let caption = `${eg}
+┃ 𓃠 *${gt} ${vs}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 💫 الاسم
+┃ ${name}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 💪 الصيغه 
+┃ ${size}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 🚀 الحجم
+┃ ${date}
+┃ ${mime}`.trim()
+conn.reply(m.chat, caption, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝑻𝑶𝑼𝑲𝑨 𝑩𝑶𝑻 🌸',
+body: 'Super Bot WhatsApp',         
+previewType: 0, thumbnail: fs.readFileSync("./vmenus/Menu3.jpg"),
+sourceUrl:`https://youtube.com/@venomov_12`}})
+   await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true })
+} catch (e) {
+m.reply(`${fg} _*نسيت تحط الينك*_`)
+handler.limit = false      
+}}
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = ['ميدفاير', 'ميديا']
+handler.register = true
+handler.limit = true
+export default handler
+
+async function mediafireDl(url) {
+const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/','')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`)
+const $ = cheerio.load(res.data)
+const link = $('#downloadButton').attr('href')
+const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ','').replaceAll('\n','')
+const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text()
+const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ','')
+let mime = ''
+let rese = await axios.head(link)
+mime = rese.headers['content-type']
+return { name, size, date, mime, link }
+}
