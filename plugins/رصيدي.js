b0100602ec879dc6d28e1085a56e3007dee9d6d3
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 10:52:39 +0200

    Create رصيدي.js

---
+++ plugins/رصيدي.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+let handler = async (m, { usedPrefix, conn}) => {	
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+else who = m.sender
+let name = conn.getName(who) 
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let grupos = [nna, nn, nnn, nnnt]
+let gata = [img5, img6, img7, img8, img9]
+let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🛡️', body: 'support group' , sourceUrl: grupos.getRandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
+let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝑻𝑶𝑼𝑲𝑨-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(img)).buffer(), sourceUrl: yt }}}
+let dos = [enlace, enlace2]
+
+let user = global.db.data.users[who]
+let premium = user.premium
+const cartera = {
+    economia: {
+    exp: true,
+    limit: true,
+    money: true,
+  },
+}
+const recursos = Object.keys(cartera.economia).map(v => user[v] && `*${global.rpgshop.emoticon(v)} ⇢ ${user[v]}*`).filter(v => v).join('\n').trim()
+let cart = `🎟️ بريميم ⇢ ${premium ? '✅' : '❌'}\n${wm}\n\n👝 ⇢ ${name}\n${recursos}\n\n`
+await conn.sendFile(m.chat, gata, 'venom.jpg', cart, fkontak )
+
+}
+handler.help = ['bal']
+handler.tags = ['xp']
+handler.command = ['رصيدي', 'cartera', 'wallet', 'cartera2', 'balance2'] 
+export default handler
