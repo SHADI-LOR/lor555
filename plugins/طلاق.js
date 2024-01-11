a44aa688fcea8449262e80ff86092a2b3ca3c2a0
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 11:02:20 +0200

    Create طلاق.js

---
+++ plugins/طلاق.js
@@ -0,0 +1,17 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)} , ${toM(b)}*
+*انتم ما تناسبون لبعض*
+ \n*تم طلاقكم 👩🏼‍⚖️*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['الطلاق','طلاق']
+handler.group = true
+export default handler
