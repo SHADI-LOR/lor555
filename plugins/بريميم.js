1dd0db3cd0f6eba607988af1c2c0d7cc535b5364
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 10:27:25 +0200

    Create بريميم.js

---
+++ plugins/بريميم.js
@@ -0,0 +1,75 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else who = m.chat
+
+//let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
+//let res = [];
+let user = global.db.data.users[who]
+if (!who) throw `*الإشارة أو الرد على رسالة الشخص الذي سيكون صاحب العضوية المميزة*\n\n*${usedPrefix + command} @${m.sender.split`@`[0]} 1*\n*${usedPrefix + command} 1*`
+let txt = text.replace('@' + who.split`@`[0], '').trim()
+let name = await '@' + who.split`@`[0]
+
+var hora1 = 3600000 * txt //1h
+var dia1 = 86400000 * txt //1d
+var semana1 = 604800000 * txt //1s
+var mes1 = 2629800000 * txt //1m
+var now = new Date() * 1
+
+if (!txt && !m.quoted) throw `*الوقت المميز مفقود*`
+if (txt == 0 || txt == null) throw `*الإشارة أو الرد على رسالة الشخص الذي سيكون صاحب العضوية المميزة*\nمثال\n*${usedPrefix + command} @${m.sender.split`@`[0]} 1*`
+if (isNaN(txt)) return m.reply(`*عدد فقط*\n\n*${usedPrefix + command} @${m.sender.split`@`[0]} 1*`)
+
+
+
+
+if (command == 'بريميم' || command == 'userpremium') {
+if (now < user.premiumTime) user.premiumTime += hora1
+else user.premiumTime = now + hora1
+user.premium = true
+m.reply(`*🎟️ المستخدم الان مميز!!!*
+
+*✨ المستخدم » ${name}*
+*🕐 المدة » ${hora1 } hora(s)*
+*📉 التوقيت » ${user.premiumTime - now} seg*`)}
+
+if (command == 'addprem2' || command == 'userpremium2') {
+if (now < user.premiumTime) user.premiumTime += dia1
+else user.premiumTime = now + dia1
+user.premium = true
+m.reply(`*🎟️ 𝙐𝙎𝙏𝙀𝘿 𝘼𝙃𝙊𝙍𝘼 𝙀𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈!!!*
+
+*✨ 𝙉𝙊𝙈𝘽𝙍𝙀 : 𝙐𝙎𝙀𝙍 » ${name}*
+*🕐 𝙏𝙄𝙀𝙈𝙋𝙊 : 𝙏𝙄𝙈𝙀 » ${dia1} Día(s)*
+*📉 𝙏𝙄𝙈𝙀𝙍 » ${user.premiumTime - now} seg*`)}
+
+
+if (command == 'addprem3' || command == 'userpremium3') {
+if (now < user.premiumTime) user.premiumTime += semana1
+else user.premiumTime = now + semana1
+user.premium = true
+m.reply(`*🎟️ 𝙐𝙎𝙏𝙀𝘿 𝘼𝙃𝙊𝙍𝘼 𝙀𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈!!!*
+
+*✨ 𝙉𝙊𝙈𝘽𝙍𝙀 : 𝙐𝙎𝙀𝙍 » ${name}*
+*🕐 𝙏𝙄𝙀𝙈𝙋𝙊 : 𝙏𝙄𝙈𝙀 » ${semana1} Semana(s)*
+*📉 𝙏𝙄𝙈𝙀𝙍 » ${user.premiumTime - now} seg*`)}
+
+
+if (command == 'addprem4' || command == 'userpremium4') {
+if (now < user.premiumTime) user.premiumTime += mes1
+else user.premiumTime = now + mes1
+user.premium = true
+m.reply(`*🎟️ 𝙐𝙎𝙏𝙀𝘿 𝘼𝙃𝙊𝙍𝘼 𝙀𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈!!!*
+
+*✨ 𝙉𝙊𝙈𝘽𝙍𝙀 : 𝙐𝙎𝙀𝙍 » ${name}*
+*🕐 𝙏𝙄𝙀𝙈𝙋𝙊 : 𝙏𝙄𝙈𝙀 » ${mes1} Mes(es)*
+*📉 𝙏𝙄𝙈𝙀𝙍 » ${user.premiumTime - now} seg*`)}
+}
+handler.help = ['addprem [@user] <days>']
+handler.tags = ['owner']
+handler.command = ['بريميم', 'userpremium', 'addprem2', 'userpremium2', 'addprem3', 'userpremium3', 'addprem4', 'userpremium4'] 
+handler.group = true
+handler.owner = true
+handler.botAdmin = true
+export default handler
