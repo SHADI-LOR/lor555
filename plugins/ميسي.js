7e373e2af89ab32e2dcdef5b82adc5e6f485f1ea
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 11:24:47 +0200

    Create ميسي.js

---
+++ plugins/ميسي.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*Messi 🇦🇷*`, m)}
+//conn.sendButton(m.chat, "*Messi 🇦🇷*", author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], m)}
+handler.help = ['messi']
+handler.tags = ['internet']
+handler.command = /^(ميسي)$/i
+handler.limit = 2
+export default handler
