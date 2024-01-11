6fb4e8e83453e46be81cc3e4370ad87fd051c568
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 09:47:58 +0200

    Create _فكي.js

---
+++ plugins/_فكي.js
@@ -0,0 +1,46 @@
+//ESTOS DISEÑOS PARA MENSAJES SON COMPATIBLES CON GATABOT, SIN EMBARGO ALGUNOS NO SE VEN EN LA VERSIÓN DE WHATSAPP WEB
+//PERO EN EL CELULAR SI SE NOTA TODOS, PUEDES USAR EL QUE QUIERAS 😼
+//import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+let generateWAMessageFromContent = (await import(global.baileys)).default
+import fs from 'fs'
+import fetch from 'node-fetch'
+import moment from 'moment-timezone' 
+
+let handler = async (m, { conn, args }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+let name = await conn.getName(who)
+let fsizedoc = '1'.repeat(10)
+let a = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BOV','BRL','BSD','BTN','BWP','BYR','BZD','CAD','CDF','CHE','CHF','CHW','CLF','CLP','CNY','COP','COU','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','INR','IQD','IRR','ISK','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRO','MUR','MVR','MWK','MXN','MXV','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS','SRD','SSP','STD','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYI','UYU','UZS','VEF','VND','VUV','WST','XAF','XAG','XAU','XBA','XBB','XBC','XBD','XCD','XDR','XFU','XOF','XPD','XPF','XPT','XTS','XXX','YER','ZAR','ZMW']
+let b = a[Math.floor(Math.random() * a.length)]
+//let ucapan = ucapan()
+let sap = ['Hai', 'Ohayo', 'Kyaa', 'Halo', 'Nyann']
+let sgh = md
+let sgc = nnn
+let gata = 'https://i.imgur.com/EXTbyyn.jpg'
+//let logo = 'https://i.pinimg.com/564x/f7/d2/e4/f7d2e48fd59a8c01cd396bfc70b0a2d1.jpg'
+let pp = await conn.profilePictureUrl(who).catch(_ => gata)
+//pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
+
+
+
+}
+handler.command = /^(fake)$/i
+export default handler
+
+function ucapan() {
+  const time = moment.tz('America/Los_Angeles').format('HH')  //America/Los_Angeles  Asia/Jakarta   America/Toronto
+  let res = "👋 *BIENVENIDO(A) | WELCOME* 👋"
+  if (time >= 4) {
+    res = "🌇 *Buenos Días | Good Morning* ⛅"
+  }
+  if (time >= 11) {
+    res = "🏙️ *Buenas Tardes | Good Afternoon* 🌤️"
+  }
+  if (time >= 15) {
+    res = "🌆 *Buenas tardes | Good Afternoon* 🌥️"
+  }
+  if (time >= 17) {
+    res = "🌃 *Buenas noches | Good Evening* 💫"
+  }
+  return res
+} 
