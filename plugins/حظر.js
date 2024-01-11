b37a0bbc2729526f917336581272121dc4b824fd
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 10:45:20 +0200

    Create حظر.js

---
+++ plugins/حظر.js
@@ -0,0 +1,24 @@
+/*let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = false
+m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊 🥳\n𝘼𝙃𝙊𝙍𝘼 𝙎𝙄 𝙀𝙎𝙏𝙊𝙔 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀.\n\n𝙏𝙃𝙄𝙎 𝘾𝙃𝘼𝙏 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 😌\n𝙉𝙊𝙒 𝙄 𝘼𝙈 𝘼𝙑𝘼𝙄𝙇𝘼𝘽𝙇𝙀.`)
+}
+handler.help = ['unbanchat2']
+handler.tags = ['owner']
+handler.command = /^unbanchat$/i
+handler.rowner = true
+handler.exp = 50
+
+export default handler*/
+
+//PARA GRUPOS
+
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = true
+m.reply(`${eg} تم حظر شات بنجاح لم يعد شغال حتى يتم رفع الحظر`)
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^حظر|bangata|banchat2$/i
+handler.botAdmin = true
+handler.admin = true 
+export default handler
