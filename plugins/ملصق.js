0e5ff67a77ab90b6fd7d55c38f7de1fd6e23a0ae
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-11 11:19:34 +0200

    Create ملصق.js

---
+++ plugins/ملصق.js
@@ -0,0 +1,68 @@
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  let user = db.data.users[m.sender]
+  let time = user.lastmining + 10000 //tiempo de espera en min
+if (new Date - user.lastmiming < 10000) return await conn.reply(m.chat, `* انتظر بضع دقائق لاستخدام أمر آخر*`,  m)
+  try {
+
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/webp|image|video/g.test(mime)) {
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('╰⊱⚠️⊱ *تحذير* ⊱⚠️⊱╮\nيجي المقطع لايزيد عن ٧ ثواني')
+      let img = await q.download?.()
+      if (!img) throw `╰⊱❗️⊱ *خطأ* ⊱❗️⊱╮\nرد على صورة او مقطع لتحويله الى ملصق*`
+      let out
+      try {
+        stiker = await sticker(img, false, global.packname, global.author)
+      } catch (e) {
+        console.error(e)
+      } finally {
+      await conn.reply(m.chat, `${ar}⏳ *إنشاء ملصق ، لحظة واحدة...* 🐈`, m)
+        if (!stiker) {
+          if (/webp/g.test(mime)) out = await webp2png(img)
+          else if (/image/g.test(mime)) out = await uploadImage(img)
+          else if (/video/g.test(mime)) out = await uploadFile(img)
+          if (typeof out !== 'string') out = await uploadImage(img)
+          stiker = await sticker(false, out, global.packname, global.author)
+        }
+      }
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('URL invalido')
+    }
+  } catch (e) {
+    console.error(e)
+    if (!stiker) stiker = e
+  } finally {
+     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `⚡ 𝑽𝑬𝑵𝑶𝑴 ⚡`, mediaType: 2, sourceUrl: nn, thumbnail: imagen1}}}, { quoted: m })
+    else throw '╰⊱❗️⊱ *خطأ* ⊱❗️⊱╮\nرد على صورة او مقطع لتحويله الى *.ملصق*'
+  }
+user.lastmiming = new Date * 1
+}
+handler.help = ['sticker']
+handler.tags = ['sticker']
+handler.command = ['s', 'sticker', 'ملصق'] 
+handler.limit = 1
+export default handler
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+
+return minutes + " m y " + seconds + " s " 
+}
+
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
