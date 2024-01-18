33b21bffce781dd3e4613fe097d7db83dac4ad8f
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-18 16:49:51 +0200

    Update plugins

---
+++ plugins/__الذكاء.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        let info1 = `
+${global.sham}
+⩥💜│قـسـم الـذكـاء الاصطناعي│💜⩤
+${global.sham}
+${global.amrr2}فينوم
+${global.amrr2}سوكونا
+${global.amrr2}بارد
+${global.amrr2}ارسم
+${global.amrr2}سيري
+${global.amrr2}تحدث
+${global.amrr2}شوف
+${global.sham}
+`;
+
+        const imageURL1 = 'https://telegra.ph/file/6190bd02915140738ed38.jpg';
+
+        await conn.sendMessage(m.chat, { image: { url: imageURL1 }, caption: info1 });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+handler.command = /^الذكاء$/i;
+handler.owner = true;
+
+export default handler;
