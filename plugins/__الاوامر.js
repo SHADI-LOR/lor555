6a47945a121f675f9688f1411e53d29573bdf992
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-19 11:16:15 +0200

    Update __الاوامر.js

--- plugins/__الاوامر.js
+++ plugins/__الاوامر.js
@@ -1,4 +1,3 @@
-
 import fs, { promises } from 'fs'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
@@ -16,160 +15,38 @@ let taguser = conn.getName(m.sender)
 let user = global.db.data.users[m.sender]
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
   //m.react('🎮');
-    await conn.sendMessage(m.chat, { react: { text: '🍇', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '🍧', key: m.key } })
     let vennom = '+201554582851'
 let menu = `
-${global.sham}
-${global.ippaa} *اهلا بك @${m.sender.split("@")[0]}*
-${global.ippaa} *اسمي : ${packname}${conn.user.jid == global.conn.user.jid ? '' : `\n*⇢ *@${global.conn.user.jid.split`@`[0]}*`}*
-${global.ippaa} *الوقت : ${uptime}*
-${global.ippaa} *اليوم : ${week}*
-${global.ippaa} *التاريخ : ${date}*
-${global.ippaa} *مطوري : ${vennom}
-${global.sham}
-قـائـمـه بـوت تـوكـا ⁦(⁠◕⁠ᴗ⁠◕⁠✿⁠)⁩
-${global.sham}
-⩥💜│الاسـاسـيـات│💜⩤
-${global.sham}
-${global.amrr}صلح
-${global.amrr}ابلاغ
-${global.amrr}المطور
-${global.amrr}المملكه
-${global.amrr}الاسقبال
-${global.amrr}تعليم
-${global.amrr}الدعم
-${global.amrr}قائمتي
-${global.amrr}لوجوهات
-${global.amrr}الجروبات
-${global.amrr}البوتات
-${global.sham}
-⩥💜│الـذكـاء الاصطناعي/ia│💜⩤
-${global.sham}
-${global.amrr}فينوم
-${global.amrr}سوكونا
-${global.amrr}ارسم
-${global.amrr}تحدث
-${global.amrr}سيري
-${global.sham}
-⩥💜│الـجـروبـ│💜⩤
-${global.sham}
-${global.amrr}اضف
-${global.amrr}طرد
-${global.amrr}ترقيه
-${global.amrr}خفض
-${global.amrr}تغير-الاسم
-${global.amrr}تغير-الوصف
-${global.amrr}تغير-الصوره
-${global.amrr}منشن
-${global.amrr}مخفي
-${global.amrr}جروب قفل
-${global.amrr}جروب فتح
-${global.amrr}الجروب
-${global.amrr}الادمن
-${global.amrr}حذف
-${global.sham}
-⩥💜│الالـعـاب│💜⩤
-${global.sham}
-${global.amrr}اكس
-${global.amrr}حظ
-${global.amrr}رياضه
-${global.amrr}دين
-${global.amrr}خمن
-${global.amrr}انمي
-${global.amrr}كت
-${global.amrr}فكك
-${global.amrr}ايمواجي
-${global.amrr}رتب
-${global.amrr}علم
-${global.amrr}عين
-${global.amrr}احزر
-${global.amrr}كوره
-${global.amrr}شخصية
-${global.amrr}الحب
-${global.amrr}الكره
-${global.amrr}زواج
-${global.amrr}طلاق
-${global.amrr}قدوتي
-${global.amrr}لعبة
-${global.amrr}موتتي
-${global.sham}
-⩥💜│الاعـضـاء│💜⩤
-${global.sham}
-${global.amrr}تطقيم
-${global.amrr}طقم
-${global.amrr}طقمي
-${global.amrr}بروفايل
-${global.amrr}خلفيه
-${global.amrr}ميمز
-${global.amrr}سرعه
-${global.amrr}ترجم
-${global.amrr}زخرفه
-${global.amrr}دمج
-${global.amrr}ملصق
-${global.amrr}سرقه
-${global.amrr}توب
-${global.amrr}جوده
-${global.amrr}تليجراف
-${global.amrr}لصوره
-${global.amrr}لفيديو
-${global.amrr}لانمي
-${global.amrr}ميسي
-${global.amrr}رونالدو
-${global.amrr}كيوت
-${global.sham}
-⩥💜│التـحـمـيـل│💜⩤
-${global.sham}
-${global.amrr}يوتيوب
-${global.amrr}فيديو
-${global.amrr}اغنيه
-${global.amrr}تيك
-${global.amrr}فيسبوك
-${global.amrr}تطبيق
-${global.amrr}بحث
-${global.amrr}صوره
-${global.amrr}جوجل
-${global.sham}
-⩥💜│الـبـنـك│💜⩤
-${global.sham}
-${global.amrr}راتب
-${global.amrr}يومي 
-${global.amrr}اسبوعي
-${global.amrr}شهري
-${global.amrr}هديتي
-${global.amrr}تحويل
-${global.amrr}لفل
-${global.amrr}خبرات
-${global.amrr}رصيدي
-${global.amrr}الماسي
-${global.sham}
-⩥💜│الاصـوات│💜⩤
-${global.sham}
-${global.amrr}رفيع
-${global.amrr}تخين
-${global.amrr}تخينن
-${global.amrr}صاخب
-${global.amrr}ناعم
-${global.amrr}سنجاب
-${global.amrr}روبوت
-${global.amrr}منفوخ
-${global.amrr}عميق 
-${global.amrr}بطيء
-${global.amrr}سريع
-${global.sham}
-⩥💜│حصرياً│💜⩤
-${global.sham}
-${global.amrr}دونات
-${global.amrr}لقب
-${global.amrr}لقبه
-${global.amrr}الألقاب
-${global.amrr}جيب
-${global.amrr}ابعت
-${global.amrr}غير
-${global.sham}
-
+*🛡️TOUKA-BOT-MÐ🛡️*
+┐┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┌ـ
+┊💜 مـرحـبـاً ♡ -  @${m.sender.split("@")[0]
+┊⏲️ وقت التشغيل♡ - ${uptime}
+┊🌄 الـيـوم ♡ - ${week}
+┊📍الـتـاريـخ ♡ - ${date}
+┊⚡مـطـوري ♡ - ${venom}
+┘┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⟢ـ
+┐┈⟤「قـائـمـات اوامــر بـوت تـوكــا ⟥┈┌
+┊🕋♡ - 「.القران
+┊📚♡ - 「.الذكاء
+┊🛡️♡ - 「.المطور
+┊💌♡ - 「.الاساسيات 
+┊🧬♡ - 「.المجموعات
+┊📥♡ - 「.التحميلات
+┊🧰♡ - 「.التحويلات
+┊🎨♡ - 「.الاستيكرات
+┊🎉♡ - 「.الحصري 
+┊🎮♡ - 「.الالعاب
+┊🎩♡ - 「.الوجوهات
+┊🪨♡ - 「.الاصوات
+┊ *.الاوامر2    > لعرض الاوامر بشكل كامل*
+┊ *.الاوامر3    > لعرض الاوامر بشكل جديد*
+│ تحت إدارة :  ♡🏎️𓆩𝑻𝒆𝒔𝒍𝒂𓆪🏎️♡
+┘┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⟢ـ
+📚 *_تـحـذيـر مـمـنـوع اسـتـخـدام الـبـوت الا فـي الجـروبـات إذا قـمـت بـدخـول الـي الـبـوت خـاص سـوف يـتـم حـظـرك_* 📚
 `
 
-const vi = ["https://telegra.ph/file/f0126c274f6d29aa2b2b1.jpg","https://telegra.ph/file/7fb53b7f8b20dfdeaad98.jpg","https://telegra.ph/file/60a402cf7b0b3564c8f73.jpg"]
+const vi = "https://telegra.ph/file/ce540d877b5a6e3e5a5d1.jpg"
 
 try {
 await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
@@ -191,7 +68,7 @@ await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() 
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}}
 
-handler.command = /^2اوامر2|الاوامر$/i
+handler.command = /^اوامر|الاوامر$/i
 handler.limit = true 
 export default handler
 
@@ -200,3 +77,4 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
