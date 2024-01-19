a472f70c8ea34936fcb785a6c76fd66db846594f
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-19 04:10:31 +0200

    Update plugins

---
+++ plugins/__التنزيلات.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        let info4 = `
+${global.sham}
+⩥💜│قـسـم الـجـروبـات│💜⩤
+${global.sham}
+${global.amrr5}فيديو
+${global.amrr5}اغنيه
+${global.amrr5}فيديوهات
+${global.amrr5}اغاني
+${global.amrr5}يوتيوب
+${global.amrr5}يوتيوب2
+${global.amrr5}تطبيق
+${global.amrr5}فيسبوك
+${global.amrr5}تيك
+${global.amrr5}انستا
+${global.sham}
+`
+        const imageURL4 = 'https://telegra.ph/file/d654286890710d451732f.jpg';
+
+        await conn.sendMessage(m.chat, { image: { url: imageURL4 }, caption: info4 });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+handler.command = /^التنزيلات$/i;
+
+export default handler;
