eb51ab5a157de0b0500a2260c51cf3c82987fbbb
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-19 10:45:22 +0200

    Update plugins

---
+++ plugins/__حصري.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        let info11 = `
+${global.sham}
+⩥💜│قـسـم الـجـديـد│💜⩤
+${global.sham}
+${global.amrr7}اكس
+${global.amrr7}حظ
+${global.amrr7}رياضه
+${global.amrr7}دين
+${global.amrr7}خمن
+${global.amrr7}انمي
+${global.amrr7}كت
+${global.amrr7}فكك
+${global.amrr7}ايمواجي
+${global.amrr7}رتب
+${global.amrr7}علم
+${global.amrr7}عين
+${global.amrr7}احزر
+${global.amrr7}كوره
+${global.amrr7}شخصية
+${global.amrr7}الحب
+${global.amrr7}الكره
+${global.amrr7}زواج
+${global.amrr7}طلاق
+${global.amrr7}قدوتي
+${global.amrr7}لعبة
+${global.amrr7}موتتي
+${global.sham}
+`;
+
+        const imageURL11 = 'https://telegra.ph/file/6190bd02915140738ed38.jpg';
+
+        await conn.sendMessage(m.chat, { image: { url: imageURL11 }, caption: info11 });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+handler.command = /^حصري$/i;
+
+export default handler;
