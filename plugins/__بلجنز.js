c49e91077657b665a3adf1e1c50b0d2acf859188
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-23 12:45:49 +0200

    Update plugins

---
+++ plugins/__بلجنز.js
@@ -0,0 +1,16 @@
+import fs from 'fs'
+
+let handler = async (m, { usedPrefix, command, text }) => {
+    let ar = Object.keys(plugins)
+    let ar1 = ar.map(v => v.replace('.js', ''))
+    if (!text) throw `*${mg}\nمش كده يسطا\nبال:\n${usedPrefix + command} ميمز`
+    if (!ar1.includes(text)) return m.reply(`'${text}' اختار انت بقي!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    m.reply(fs.readFileSync('./plugins/' + text + '.js', 'utf-8'))
+}
+handler.help = ['getplugin'].map(v => v + ' <teks>')
+handler.tags = ['host']
+handler.command = /^(موارد-بلجنز)$/i
+
+handler.rowner = true
+
+export default handler
