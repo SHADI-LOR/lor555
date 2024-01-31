0e4ef35b388de2cc50324839facaba152e6b8cef
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-31 19:02:08 +0200

    Update __القران.js

--- plugins/__القران.js
+++ plugins/__القران.js
@@ -3,14 +3,17 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix, command }) => {
     try {
         let info14 = `
-${global.sham}
-⩥💜│قـسـم الـقـران│💜⩤
-${global.sham}
+${global.sham1}
+${global.sham2}
+│┊⩥💜│قـسـم الـقـران│💜⩤
+${global.sham3}
+${global.sham2}
 ${global.amrr11}دين
 ${global.amrr11}قران
 ${global.amrr11}الله
 ${global.amrr11}حديث
-${global.sham}
+${global.sham3}
+${global.sham4}
 `
         const imageURL14 = 'https://telegra.ph/file/9d6d7e272bc8c6c8125bc.jpg';
 
@@ -20,6 +23,6 @@ ${global.sham}
     }
 };
 
-handler.command = /^القران$/i;
+handler.command = /^القران|ت3$/i;
 
 export default handler;
