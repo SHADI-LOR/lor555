9f135cc872909c15d5d47e5139c640268a04423b
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-01-31 03:35:43 +0200

    Update plugins

---
+++ plugins/_استيكرات.js
@@ -0,0 +1,43 @@
+import { sticker } from '../lib/sticker.js'
+
+let handler = async(m, { conn }) => {
+    const venomStickers = [
+   "https://telegra.ph/file/dd6ad352f751ca003fd47.jpg",
+"https://telegra.ph/file/a82fe14347a4c8dcacf48.jpg"
+    ];
+
+    const sukunaStickers = [
+        "https://telegra.ph/file/33e985b44ac9da14ba466.jpg", 
+    ];
+const ssaStickers = [
+        "https://telegra.ph/file/dc7035b3c4573c4fc476f.jpg",
+    ];
+const ssa2Stickers = [
+        "https://telegra.ph/file/331bf5cd0f4de3a4928f4.jpg",
+"https://telegra.ph/file/6ea87d1040a5e34e40c7a.jpg"
+    ];
+
+    let text = m.text.toLowerCase(); 
+    if (text.includes("حزين")||text.includes("زعلان")) {
+        let stiker = await sticker(null, venomStickers[Math.floor(Math.random() * venomStickers.length)]);
+        if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+    }
+    if (text.includes("مساء الخير")||text.includes("مساء الاناناس")) {
+        let stiker = await sticker(null, sukunaStickers[Math.floor(Math.random() * sukunaStickers.length)]);
+        if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+    }
+    if (text.includes("انا جيت")||text.includes("جيت")) {
+        let stiker = await sticker(null, ssaStickers[Math.floor(Math.random() * ssaStickers.length)]);
+        if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+    }
+    if (text.includes("كل تبن")||text.includes("كل خرا")||text.includes("كل زق")) {
+        let stiker = await sticker(null, ssa2Stickers[Math.floor(Math.random() * ssa2Stickers.length)]);
+        if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+    }
+}
+
+handler.customPrefix = /مساء الخير|زعلان|كل خرا|كل زق|انا جيت|كل تبن|جيت|حزين/i
+handler.command = new RegExp
+handler.exp = 50
+export default handler
+
