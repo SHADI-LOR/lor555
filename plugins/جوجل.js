b1b2b5a5ee015588d51af7f2779ef8324508e236
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-02-16 09:27:50 +0200

    Update plugins

---
+++ plugins/جوجل.js
@@ -0,0 +1,29 @@
+import {googleIt} from '@bochilteam/scraper';
+import google from 'google-it';
+import axios from 'axios';
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+const fetch = (await import('node-fetch')).default;
+const text = args.join` `;
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()} ${mid.smsMalused} *${usedPrefix + command} Cat*`, m)
+try {
+const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+google({'query': text}).then(res => {
+let teks = `🔍 ${mid.buscador9} ${text}\n\n*${url}*\n\n`
+for (let g of res) {
+teks += `_${g.title}_\n_${g.link}_\n_${g.snippet}_\n\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n\n`
+} 
+const ss = `https://image.thum.io/get/fullpage/${url}`
+conn.sendFile(m.chat, ss, 'error.png', teks, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
+//m.reply(teks)
+})
+} catch {    
+handler.limit = 0
+}}
+handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.tags = ['internet']
+handler.command = /^googlef|جوجل?$/i
+handler.exp = 40
+handler.exp = 3
+handler.limit = 1
+export default handler
