e57026daa5d430acc417cfe7f222b2ec0cff3400
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-02-17 02:24:44 +0200

    Update __الاوامر.js

--- plugins/__الاوامر.js
+++ plugins/__الاوامر.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/43c7d75c72e184a6569f0.mp4';
+let videoUrl = 'https://telegra.ph/file/7e9b388a0f8022dce75fc.mp4';
  let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
