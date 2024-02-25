2f8813b6cc76c96b77dc0a6e0440b160bedbaf91
Xov445447533 <153835525+Xov445447533@users.noreply.github.com>
2024-02-25 16:53:36 +0200

    Update استيكر.js

--- plugins/استيكر.js
+++ plugins/استيكر.js
@@ -7,44 +7,45 @@ let handler = async (m, {
     usedPrefix,
     command
 }) => {
-let text
+    let text
     if (args.length >= 1) {
         text = args.slice(0).join(" ")
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text
     } else throw "╰⊱❗️⊱ *ضيف عليه نص عشان يسوي لك ملصق مع افتار حقك واذا تبي ملصق عاد قول .ملصق "
-   if (!text) return m.reply('والنص?')
-   if (text.length > 30) return m.reply('30 كلمة كحد الاقصى!')
+    if (!text) return m.reply('والنص؟')
+    if (text.length > 30) return m.reply('30 كلمة كحد الاقصى!')
+
     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg')
 
-   const obj = {
-      "type": "quote",
-      "format": "png",
-      "backgroundColor": "#000000",
-      "width": 512,
-      "height": 680,
-      "scale": 2,
-      "messages": [{
-         "entities": [],
-         "avatar": true,
-         "from": {
-            "id": 1,
-            "name": m.name,
-            "photo": {
-               "url": pp
-            }
-         },
-         "text": text,
-         "replyMessage": {}
-      }]
-   }
-   const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
-      headers: {
-         'Content-Type': 'application/json'
-      }
-   })
-   const buffer = Buffer.from(json.data.result.image, 'base64')
-   let stiker = await sticker(buffer, false, global.packname, global.author)
+    const obj = {
+        "type": "quote",
+        "format": "png",
+        "backgroundColor": "#000000",
+        "width": 256, // تم تغيير العرض إلى 256
+        "height": 340, // تم تغيير الطول إلى 340
+        "scale": 2,
+        "messages": [{
+            "entities": [],
+            "avatar": true,
+            "from": {
+                "id": 1,
+                "name": m.name,
+                "photo": {
+                    "url": pp
+                }
+            },
+            "text": text,
+            "replyMessage": {}
+        }]
+    }
+    const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const buffer = Buffer.from(json.data.result.image, 'base64')
+    let stiker = await sticker(buffer, false, global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'Quotly.webp', '', m)
 }
 
