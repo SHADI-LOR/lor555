8c02f74877e03c49d6e51d7fc0555f3a2beb2c9a
SHADI-LOR <144344909+SHADI-LOR@users.noreply.github.com>
2024-05-19 15:51:31 +0300

    منشن.js

--- plugins/منشن.js
+++ plugins/منشن.js
@@ -2,13 +2,13 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, comma
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
 throw false
-}
-let pesan = args.join` `
-let oi = `*🌸~الرساله:* ${pesan}`
-let teks = `*منشن 🌸 جماعي* \n\n ${oi}\n\n*🌿┇الجروب :⇣*\n`
+}//غير ڤينوم و حط اسم بوتك
+let pesan = args.join`*⎔⋅• ━ ╼╃ ⌬〔🕸️〕⌬ ╄╾ ━ •⋅⎔*`
+let oi = `*💭|* ${pesan}`
+let teks = ` \n\n ${oi}\n\n*🗡┇الاعضاء:⇣*\n`
 for (let mem of participants) {
-teks += `*🌸↫* @${mem.id.split('@')[0]}\n`}
-teks += `*🅣🅞🅤🅚🅐 🅑🅞🅣*`
+teks += `*🕸️↫* @${mem.id.split('@')[0]}\n`}
+teks += `*𝚂𝚁 𝙻𝙾𝚁 𝙱𝙾𝚃*\n\n*⎔⋅• ━ ╼╃ ⌬〔🕸️〕⌬ ╄╾ ━ •⋅⎔*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
@@ -16,4 +16,4 @@ handler.tags = ['group']
 handler.command = /^(منشن|invocar|invocacion|todos|invocación)$/i
 handler.admin = true
 handler.group = true
-export default handler
+export default handler{
