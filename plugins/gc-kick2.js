35718a664a90ad7539f3140299e7b0b104467166
SHADI-LOR <144344909+SHADI-LOR@users.noreply.github.com>
2024-05-20 18:57:31 +0300

    طرد2.js

--- plugins/gc-kick2.js
+++ plugins/gc-kick2.js
@@ -1,18 +1,18 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `[⚡]~ ما تنسي المنشن`
+let kickte = `[🕸️]~ ما تنسي المنشن`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`[⚡]⌯ بــرا زي الـكـلـب`) 
+m.reply(`[👋]⌯ نشوفك على خير ان شاء الله`) 
 }
 
 handler.help = ['S H A D O W']
 handler.tags = ['S H A D O W']
-handler.command = ['طرد'] 
+handler.command = ['طرد' 'طلعيه'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
