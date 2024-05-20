2bdb92bd946bb16c40da30e637d820e6c93c5ae2
SHADI-LOR <144344909+SHADI-LOR@users.noreply.github.com>
2024-05-20 18:24:54 +0300

    ايدي.js

--- plugins/hina-id.js
+++ plugins/hina-id.js
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`[⚡]⌯ هـــذا هــو الــايــدي الــخــاص بــك ↯`.trim())
+await m.reply(`[🪪]⌯ هـــذا هــو الــايــدي الــخــاص بــك ↯`.trim())
 await m.reply(`${sn}`.trim())
 }
 handler.help = ['myns']
