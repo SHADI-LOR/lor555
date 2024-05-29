1d527d5a4c5a7f072c83da36d45b1b68f70be3dd
SHADI-LOR <144344909+SHADI-LOR@users.noreply.github.com>
2024-05-30 00:36:40 +0300

    Create اشتري.js

---
+++ plugins/اشتري.js
@@ -0,0 +1,27 @@
+const xpperlimit = 320;
+const handler = async (m, {conn, command, args}) => {
+  let count = command.replace(/^شراء/i, '');
+  count = count ? /الكل/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count;
+    global.db.data.users[m.sender].limit += count;
+    conn.reply(m.chat, `
+┌─「 الدفع 」
+⌯ الكيمة : + ${count} 💎 
+⌯ الفلوس : -${xpperlimit * count} XP
+└──────────────`, m);
+  } else conn.reply(m.chat, `[❣️]~ اسفة ماعندك خبرة عشان تشتري ${count} ماس 💎`, m);
+return conn.sendMessage(m.chat, {
+react: {
+  text: '💎',
+  key: m.key,
+}})
+};
+handler.help = ['S H A D O W'];
+handler.tags = ['S H A D O W'];
+handler.command = ['شراء', 'شراءالكل'];
+
+handler.disabled = false;
+
+export default handler;
